21b96d28aa9cb9a1a1a1bdb157c6409fb56b1e3e
ElfQueen <emmaparsley98@gmail.com>
2026-05-25 20:22:23 +0100

    Random colour on game start

--- UnityGoblins/Assets/Scripts/Player/PlayerColourComponent.cs
+++ UnityGoblins/Assets/Scripts/Player/PlayerColourComponent.cs
@@ -6,29 +6,26 @@ using UnityEngine.InputSystem;
 public class PlayerColourComponent : NetworkBehaviour
 {
     MeshRenderer _meshRenderer;
-    InputAction _attackAction;
 
-    [Networked, OnChangedRender(nameof(ColourChanged))] Color NetworkedColour {  get; set; }
+    [Networked, OnChangedRender(nameof(ColourChanged))] public Color NetworkedColour {  get; set; }
 
 
     public override void Spawned()
     {
         _meshRenderer = GetComponent<MeshRenderer>();
-        if (!HasStateAuthority) return;
+        
+        if (HasStateAuthority)
+        {
+            NetworkedColour = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
+        }
 
-        _attackAction = InputSystem.actions.FindAction("Use");
+        ColourChanged();
     }
 
 
     private void Update()
     {
         if (!HasStateAuthority) return;
-
-        var attackValue = _attackAction.ReadValue<float>();
-        if (attackValue > 0)
-        {
-            NetworkedColour = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
-        }
     }
 
     private void ColourChanged()
